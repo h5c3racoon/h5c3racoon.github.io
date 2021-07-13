3a2a8fa861037c8d6c16704afa1b919591ea6ed2
h5c3racoon <h5c3racoon@gmail.com>
2021-07-13 14:24:54 +0400

    add mini_game XD

---
+++ source/etc/mini__game/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $('.counter__span').text(10);
+});
+
+$('.game__grid > div').click(function(){
+
+  var counter = $('.counter__span').text();
+
+  if(counter == 0) {
+    alert('Попытки закончелись!')
+  }
+  
+  else {
+    $('.counter__span').text(counter - 1);
+
+    var coordinates = $(this).attr('data-coordinates');
+    if(coordinates == 'h10') {
+      alert('Верно, это телефон!');
+    }
+    if(coordinates == 'f6') {
+      alert('Верно, это канцелярские иголки!');
+    }
+    console.log(coordinates);
+  }
+
+});
+
+function hideGridLine() {
+  $('.game__grid > div').css('border', 'none');
+  $('.game__grid > div').css('background', 'none');
+};
+
+function showGridLine() {
+  $('.game__grid > div').css('border-bottom', '1px solid rgba(255, 255, 255, 0.3)');
+  $('.game__grid > div:nth-child(2n)').css('background', 'rgba(255, 255, 255, 0.3)');
+};
