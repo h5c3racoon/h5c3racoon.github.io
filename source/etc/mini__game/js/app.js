fb730aaeb8dd76713d4c54223dbf92c280bb5d11
h5c3racoon <h5c3racoon@gmail.com>
2021-07-14 18:33:05 +0400

    change mini_game (vue)

---
+++ source/etc/mini__game/js/app.js
@@ -0,0 +1,64 @@
+const App = {
+  data() {
+    return {
+      // --- data
+      alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+      cellxrow: 12,
+      coordinate_cell: [],
+      image_size: {width: 500, height: 500},
+      game_field: {},
+      increase_size: false,
+      answers: []
+    }
+  },
+  // --- mounted
+  mounted() {
+
+    for(let i = 0; i < this.cellxrow; i++) {
+      for(let q = 0; q < this.cellxrow; q++) {
+        this.coordinate_cell.push(this.alphabet[i] + [q + 1])
+      }
+    }
+
+    this.game_field = this.image_size
+
+  },
+  // --- methods
+  methods: {
+    // get coordinate
+    getCoordinate(coordinate) {
+      if(this.answers.length == 10) {
+        alert('Попытки закончелись!')
+      }
+      else {
+        console.log(coordinate)
+        this.answers.push(coordinate)
+        console.log('answers length:', this.answers.length);
+      }
+    },
+
+    // increase size
+    increaseSize() {
+      this.increase_size = !this.increase_size
+      console.log('increase_size:', this.increase_size);
+
+      if(this.increase_size == true) {
+        // let userWidth = window.innerWidth
+        let userHeight = window.innerHeight
+
+        this.game_field = {
+          width: userHeight,
+          height: userHeight
+        }
+      }
+
+      if(this.increase_size == false) {
+        this.game_field = this.image_size
+      }
+    }
+  },
+  // --- computed
+  computed: {}
+}
+
+Vue.createApp(App).mount('#app')
