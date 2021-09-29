970a27a6045331cbe1daffe6adfcdcff831382ca
h5c3racoon <h5c3racoon@gmail.com>
2021-09-29 12:24:29 +0400

    new

--- samples/reddit-search-image/js/main.js
+++ samples/reddit-search-image/js/main.js
@@ -1,15 +1,26 @@
 searchButton.addEventListener('click', () => {
 
-        let inputText = searchInput.value
-        searchInput.value = ''
+        let inputText = searchInput.value;
+        searchInput.value = '';
 
-        let items = []
-        list.innerHTML = ''
+        let items = [];
+        list.innerHTML = '';
 
-        messages.innerHTML = '<div>load data ...</div>' +
-                             '<div class="preloader lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>'
+        messages.innerHTML = `
+          <div>load data ...</div>
+          <div class="preloader lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        `;
 
-        let fetchUrl = 'https://www.reddit.com/r/' + inputText + '.json?limit=100'
+        let fetchUrl = 'https://www.reddit.com/r/' + inputText + '.json?limit=100';
 
         fetch(fetchUrl)
         .then(response => {
@@ -20,31 +31,86 @@ searchButton.addEventListener('click', () => {
             for(let item in data.data.children) {
                 let oneItem = data.data.children[item].data
                 if(oneItem.url.indexOf('.jpg') !== -1 && oneItem.url.indexOf('i.imgur.com') == -1) {
-                    items.push(oneItem)
+                    items.push(oneItem);
                 }
             }
 
-            messages.innerHTML = 'result: <span class="blue">' + items.length + '</span>'
+            messages.innerHTML = `
+              <div class="result">
+                <div class="total">
+                  result: <span class="blue"> ${items.length} </span>
+                </div>
+                <div class="grid">
+                  <span id="gridSwap" onclick="gridSwap()">only img</span>
+                </div>
+              </div>
+            `;
 
-            for(item in items) {
-                list.innerHTML += '<div class="item">' +
-                                        '<div class="title">' + items[item].title + '</div>' +
-                                        '<div class="preview">' +
-                                            '<a href="' + items[item].url + '" target="_blank">' +
-                                                '<img src="' + items[item].thumbnail + '">' +
-                                            '</a>' +
-                                        '</div>' +
-                                        '<div class="author">' +
-                                            '<a href="https://www.reddit.com/user/' + items[item].author + '" target="_blank">' +
-                                                items[item].author +
-                                            '</a>'
-                                        '</div>' +
-                                    '</div>'
-            }
+            items.map(item => {
+              list.innerHTML += `
+                <div class="nItem">
+                  <div class="nContent">
+                    <div class="nAttachImg">
+                      <a href="${item.url}" target="_blank">
+                        <img src="${item.thumbnail}" />
+                      </a>
+                    </div>
+                    <div class="nTextContent">${item.title}</div>
+                  </div>
+                  <div class="nAuthor">
+                    <a href="https://www.reddit.com/user/${item.author}" target="_blank">${item.author}</a>
+                  </div>
+                </div>
+              `
+            });
+
         })
         .catch(error => {
             list.innerHTML = ''
             messages.innerHTML = '"' + inputText + '", not fount'
         })
 
-})
+        gridSwap = () => {
+          let v = document.getElementById('gridSwap').innerText;
+          if(v === 'only img') {
+            document.getElementById('gridSwap').innerHTML = 'card view';
+            // rebuild
+            list.innerHTML = '';
+            items.map(item => {
+              list.innerHTML += `
+                <div class="onlyImgCard">
+                  <a href="${item.url}" target="_blank">
+                    <img src="${item.thumbnail}" />
+                  </a>
+                  <span>
+                    <a href="https://www.reddit.com/user/${item.author}" target="_blank">${item.author}</a>
+                  </span>
+                </div>
+              `;
+            })
+          }
+          if(v === 'card view') {
+            document.getElementById('gridSwap').innerHTML = 'only img';
+            // rebuild
+            list.innerHTML = '';
+            items.map(item => {
+              list.innerHTML += `
+                <div class="nItem">
+                  <div class="nContent">
+                    <div class="nAttachImg">
+                      <a href="${item.url}" target="_blank">
+                        <img src="${item.thumbnail}" />
+                      </a>
+                    </div>
+                    <div class="nTextContent">${item.title}</div>
+                  </div>
+                  <div class="nAuthor">
+                    <a href="https://www.reddit.com/user/${item.author}" target="_blank">${item.author}</a>
+                  </div>
+                </div>
+              `
+            });
+          }
+        }
+
+});
